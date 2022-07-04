d4036b89ebe246122e745892649bf59fbf7039ec
Skull <86374920+skkuull@users.noreply.github.com>
2022-07-04 22:22:44 +0300

    add localizations
    there is guaranteed an better way of doing that but ¯\_(ツ)_/¯

--- mwr-files-cleaner/Program.cs
+++ mwr-files-cleaner/Program.cs
@@ -59,13 +59,64 @@ namespace mwr_files_cleaner
 
             try
             {
-                Directory.Move("english\\", "zone\\english");
+                if (Directory.Exists("english"))
+                {
+                    Directory.Move("english\\", "zone\\english");
+                }
+                else if (Directory.Exists("english_safe"))
+                {
+                    Directory.Move("english_safe\\", "zone\\english_safe"); // arabic
+                }
+                else if (Directory.Exists("french"))
+                {
+                    Directory.Move("french\\", "zone\\french");
+                }
+                else if (Directory.Exists("german"))
+                {
+                    Directory.Move("german\\", "zone\\german");
+                }
+                else if (Directory.Exists("russian"))
+                {
+                    Directory.Move("russian\\", "zone\\russian");
+                }
+                else if (Directory.Exists("polish"))
+                {
+                    Directory.Move("polish\\", "zone\\polish");
+                }
+                else if (Directory.Exists("japanese_partial"))
+                {
+                    Directory.Move("japanese_partial\\", "zone\\japanese_partial");
+                }
+                else if (Directory.Exists("korean"))
+                {
+                    Directory.Move("korean\\", "zone\\korean");
+                }
+                else if (Directory.Exists("portuguese"))
+                {
+                    Directory.Move("portuguese\\", "zone\\portuguese");
+                }
+                else if (Directory.Exists("spanish"))
+                {
+                    Directory.Move("spanish\\", "zone\\spanish");
+                }
+                else if (Directory.Exists("italian"))
+                {
+                    Directory.Move("italian\\", "zone\\italian");
+                }
+                else if (Directory.Exists("simplified_chinese"))
+                {
+                    Directory.Move("simplified_chinese\\", "zone\\simplified_chinese");
+                }
+                else if (Directory.Exists("traditional_chinese"))
+                {
+                    Directory.Move("traditional_chinese\\", "zone\\traditional_chinese");
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-
+            
             Console.WriteLine("Done.");
 
             Environment.Exit(0);
